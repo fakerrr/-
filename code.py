377cee4d22137d00277ba0bf41d6424c25799cac
fakerrr <973076977@qq.com>
2018-01-27 18:04:52 +0800

    Update code.py

--- code.py
+++ code.py
@@ -1,49 +1,6 @@
 #!/usr/bin/env python
 #coding=utf-8
 
-from Tkinter import *
-import time
-import random
-
-class App:
-    def __init__(self,master):
-        #设置窗口标题
-        self.ct = master
-        self.ct.title('东安年会抽奖V1.0测试版')
-        
-        #获取屏幕高度和宽度，使窗口居中
-        screenwidth = master.winfo_screenwidth()
-        screenheight = master.winfo_screenheight() - 100
-        master.resizable(False,False)
-#         master.geometry('%sx%s+%s+%s' % (master.winfo_width() + 10, master.winfo_height() + 10, (screenwidth - master.winfo_width())/2,
-#  (screenheight - master.winfo_height())/2) )
-
-        self.main()
-
-    def main(self):
-        self.pp1 = PhotoImage(file='1.gif')
-        self.pp2 = PhotoImage(file='2.gif')
-        # pp3 = PhotoImage(file='3.jpg')
-        # pp4 = PhotoImage(file='4.jpg')
-        # pp5 = PhotoImage(file='5.jpg')
-
-        self.lable = Label(self.ct,image=self.pp1)
-        self.lable.bm = self.pp1
-        self.lable.pack(fill=X,expand=1)
-        self.lable.after(2000,self.changeImage)
-        
-
-    def changeImage(self):
-        self.lable.configure(image = self.pp2)
-
-root = Tk()
-
-app = App(root)
-
-root.mainloop()
-#!/usr/bin/env python
-#coding=utf-8
-
 from tkinter import *
 import time
 import random
@@ -53,14 +10,20 @@ class App:
     def __init__(self,master):
         #设置窗口标题
         self.ct = master
-        self.ct.title('东安年会抽奖V1.0测试版')
+        self.ct.title('东安年会抽奖V1.7最终版')
         
         #获取屏幕高度和宽度，使窗口居中
-        screenwidth = master.winfo_screenwidth()
-        screenheight = master.winfo_screenheight() - 100
-        master.resizable(False,False)
-#         master.geometry('%sx%s+%s+%s' % (master.winfo_width() + 10, master.winfo_height() + 10, (screenwidth - master.winfo_width())/2,
-#  (screenheight - master.winfo_height())/2) )
+        # screenwidth = master.winfo_screenwidth()
+        # screenheight = master.winfo_screenheight()
+        # print(screenwidth)
+        # print(screenheight)
+        # master.resizable(False,False)
+        # master.geometry('%sx%s+%s+%s' % (master.winfo_width() + 10, master.winfo_height() + 10, (screenwidth - master.winfo_width())/2,(screenheight - master.winfo_height())/2) )
+        master.maxsize(900,1000)
+        master.minsize(900,700)
+        master.bind("<Return>",self.callback)
+        master.bind("<space>",self.callback2)
+        self.pd = 1
         self.i = 1
         self.GetImage()
         self.main()
@@ -70,31 +33,65 @@ class App:
         self.lable.bm = self.a[0]
         self.lable.pack(fill=X,expand=1)
         self.lable.after(100,self.changeImage)
+        # Button(self.ct,text='开始',command=self.callback).pack()
+        # Button(self.ct,text='暂停',command=self.callback2).pack()
         # self.lable.after(2000,self.changeImage)
         
     def GetImage(self):
         
         self.a = []
-        image_list = os.listdir('id_image/')
+        self.b = []
+        image_list = os.listdir('1/')
         print(image_list)
         for x in  image_list:
-            b = PhotoImage(file='id_image/'+x)
-            self.a.append(b)
+            c = PhotoImage(file='1/'+x)
+            self.a.append(c)
+            self.b.append(c)
             print('正在装载')
+        print(self.a)
+        print(self.b)
 
     def changeImage(self):
+        print("change image"+str(self.pd))
+        if self.pd ==2 :
+            return
         while 1:
             if self.i < len(self.a):
                 self.lable.configure(image = self.a[self.i]) 
                 self.i += 1
                 self.lable.after(100,self.changeImage)
+                if self.i == len(self.a):
+                    self.i = 0
                 return
             break
                 # self.lable.configure(image = self.pp1)
 
 
+    def callback(self,event):
+        print('i got start')
+        self.pd = 1
+        self.lable.after(100,self.changeImage)
 
-        
+    def callback2(self,event):
+        self.pd = 2
+        print('i got stop')
+        self.zjr = random.randint(0,len(self.a)-1)
+        # print((self.)
+        print(self.zjr)
+        weishu = self.zjr
+        self.zjr = self.a[self.zjr]
+        print(self.zjr)
+        for i in self.b:
+            print('这是i：',i)
+            if i == self.zjr:
+                print(i)
+                print('匹配')
+                image_zjr = i
+        self.lable.configure(image = image_zjr) 
+        del self.a[weishu]
+        print('已从self.a中删除此人')
+        print('现在还有几个人：',len(self.a))
+
 
     
 root = Tk()
